9c956b1b9e7c36395aed13fd098187615d4ed1af
lejakewolf <lejakewolf@protonmail.com>
2024-02-25 12:48:00 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#FFEAD0",
+                secondary: "#F76F8E",
+                tertiary: "#96616B",
+            },
+        },
+    },
+    plugins: [],
+};
